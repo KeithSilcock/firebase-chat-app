654dfb48067824aa181e00bd71394f26f67869ce
KeithSilcock <silcockk@gmail.com>
2018-05-22 12:25:00 -0700

    created navbar, homepage, chat page

---
+++ src/components/nav.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+export default props => {
+
+    const navStyle ={
+        padding: '0 10px',
+    };
+
+    return (
+        <nav style={navStyle} className='purple lighten-2'>
+            <div className="nav-wrapper">
+                <Link to='/' className='brand-logo'> Chatty App</Link>
+
+                <ul className="right">
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/chat'>Chat</Link>
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+    )
+}
