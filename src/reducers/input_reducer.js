595133063d6353d3ab6e681178d4e957f26b2f6f
KeithSilcock <silcockk@gmail.com>
2018-06-15 18:11:26 -0700

    created functionality to log in, log out, create account display username on chat and collapse nav depending on login status

--- src/reducers/input_reducer.js
+++ src/reducers/input_reducer.js
@@ -1,19 +1,19 @@
-import types from '../actions/types';
+import types from "../actions/types";
 
 const DEFAULT_STATE = {
-    message: '',
+  message: ""
 };
 
-
-export default (state=DEFAULT_STATE, action) => {
-    switch(action.type){
-        case types.UPDATE_INPUT:
-            const {name, value} = action.payload;
-            return {...state, [name]:value};
-        case types.CLEAR_INPUT:
-            return {...state, [action.payload]:''};
-
-        default:
-            return state;
-    }
-}
+export default (state = DEFAULT_STATE, action) => {
+  switch (action.type) {
+    case types.UPDATE_INPUT:
+      const { name, value } = action.payload;
+      return { ...state, [name]: value };
+    case types.CLEAR_INPUT:
+      return { ...state, [action.payload]: "" };
+    case types.CLEAR_MANY_INPUTS:
+      return { ...state, ...action.payload };
+    default:
+      return state;
+  }
+};
